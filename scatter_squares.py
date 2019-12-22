a01b3903c07d0d8c81a69365144a4600074489d7
Klara Pauline <pauline.klara@gmail.com>
2019-12-22 09:03:43 +0100

    Styling the output with title and labels

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,5 +6,14 @@ points based on certain characteristics
 
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+plt.scatter(2, 4, s = 200)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize = 14)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Square of Value", fontsize = 14)
+
+# Set size of tick labels.
+plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
+
 plt.show()
