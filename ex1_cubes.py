8677bc23ff8a0adc0e4de49a1a26dac1f6eed063
Klara Pauline <pauline.klara@gmail.com>
2019-12-22 19:44:33 +0100

    Create ex1_cubes.py

---
+++ ex1_cubes.py
@@ -0,0 +1,17 @@
+# A number raised to the third power is a cube.
+# Plot the first five cubic numbers, and then plot the first 5000 cubic numbers
+
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 6))
+y_values = [x**3 for x in x_values]
+
+plt.scatter(x_values, y_values, s = 40)
+
+plt.title("Cubic Numbers", fontsize = 14)
+plt.xlabel("Values", fontsize = 14)
+plt.ylabel("Cubic of Numbers", fontsize = 14)
+
+plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
+
+plt.show()
