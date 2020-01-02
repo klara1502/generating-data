1f5078cda092e0621c11bd9c577f89f0dab190ac
Klara Pauline <pauline.klara@gmail.com>
2020-01-02 16:06:32 +0100

    Altering the size to fill the screen

--- rw_visual.py
+++ rw_visual.py
@@ -5,8 +5,11 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk(50000)
+    rw = RandomWalk()
     rw.fill_walk()
+
+    # Set the size of the plotting window.
+    plt.figure(figsize = (10, 6))
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues,
